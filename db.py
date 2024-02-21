eac0cab284e6e20b556b16e31c715e1ac2927adc
JorgePalaciosMel <113951878+JorgePalaciosMel@users.noreply.github.com>
2024-02-21 14:59:11 -0600

    Update db.py

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = '3306'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'x65wpxMr'
+app.config['MYSQL_PASSWORD'] = '*****'
 app.config['MYSQL_DB'] = 'db_practicas'
  
 mysql = MySQL(app)
